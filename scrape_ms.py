55ef8d6345ff3b60cb65a4f07087898ee4b2ba8a
m <msgrasser+mr-z-ro@gmail.com>
2017-03-11 14:31:04 -0500

    automate grabbing of mutual fund ticker symbols

--- scrape_ms.py
+++ scrape_ms.py
@@ -3,6 +3,7 @@
 import sys, getopt
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import requests
 
 def main(argv):
    ticker = ''
@@ -35,10 +36,19 @@ def scrape(ticker):
 
    # Write the pertinent bits to a csv
    csv = open('./{}_mfund_holders.csv'.format(ticker), 'w+')
-   csv.write('"{0}", "{1}", "{2}", "{3}", "{4}", "{5}"\n'.format("name", "shares", "percentage_of_all_shares", "change_num_shares", "change_percentage", "percent_total_assets", "date_of_portfolio"))
+   csv.write('"{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", "{7}"\n'.format("name", "shares", "percentage_of_all_shares", "change_num_shares", "change_percentage", "percent_total_assets", "date_of_portfolio", "mfund_ticker"))
    for row in soup.find(id='FundList').table.find_all('tr', class_=lambda x: x != 'hr')[1:]:
       cells = row.find_all('td')
-      csv.write('"{0}", {1}, {2}, {3}, {4}, {5}\n'.format(cells[0].text.strip().replace(',', ''), cells[1].text.strip().replace(',', ''), cells[2].text.strip().replace(',', ''), cells[3].text.strip().replace(',', ''), cells[4].text.strip().replace(',', ''), cells[5].text.strip().replace(',', ''), cells[7].text.strip().replace(',', '')))
+      
+      # Get ticker
+      mfund_ticker = ""
+      if cells[0].a:
+         ms_id = cells[0].a.get("href").split("(")[1].split("?t=")[1].split("&")[0]
+         r = requests.get("http://quote.morningstar.com/Quote/Quote.aspx?t={}&culture=en-US".format(ms_id), allow_redirects=False)
+         soup2 = BeautifulSoup(r._content, "html.parser")
+         mfund_ticker = soup2.a.get('href').split("/")[-1].split("?")[-1].split("&")[0].split("=")[1]
+      
+      csv.write('"{0}", {1}, {2}, {3}, {4}, {5}, "{6}", "{7}"\n'.format(cells[0].text.strip().replace(',', ''), cells[1].text.strip().replace(',', ''), cells[2].text.strip().replace(',', ''), cells[3].text.strip().replace(',', ''), cells[4].text.strip().replace(',', ''), cells[5].text.strip().replace(',', ''), cells[7].text.strip().replace(',', ''), mfund_ticker))
    csv.close()
 
 if __name__ == "__main__":
